debe7809b99fa9243e1f49604937816bfadea6cc
JustAPie <justapie06@gmail.com>
2022-08-24 14:12:36 +0700

    fix display error

--- src/commands/music/queueCommand.ts
+++ src/commands/music/queueCommand.ts
@@ -47,7 +47,7 @@ export default class QueueCommand {
         const selected: Song[] = this._divdQ[page];
         const embed = new MessageEmbed()
             .setTitle("Queue")
-            .setDescription(`Page: ${page}/${this._divdQ.length}`)
+            .setDescription(`Page: ${page + 1}/${this._divdQ.length}`)
             .setFooter({ text: "This list might be incorrect, please use this command again to update the queue" });
         selected.map((song: Song) => {
             embed.addField(
