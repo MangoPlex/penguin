0e0f1d81cc9c5a50864be66a4b9780da4578f94a
JustAPie <justapie06@gmail.com>
2022-09-16 19:28:12 +0700

    add support for multiple coins

--- src/main/java/xyz/mangostudio/Penguin/utils/CryptoUtils.java
+++ src/main/java/xyz/mangostudio/Penguin/utils/CryptoUtils.java
@@ -1,21 +1,43 @@
 package xyz.mangostudio.Penguin.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CryptoUtils {
     private static final OkHttpClient CLIENT = new OkHttpClient();
+    private static final List<CoinData> COIN_DATA_LIST = new ArrayList<>();
 
-    public static String formatCoin(String coin) {
-        if (coin.equalsIgnoreCase("btc")) return "bitcoin";
-        else if (coin.equalsIgnoreCase("eth")) return "ethereum";
-        else if (coin.equalsIgnoreCase("mana")) return "decentraland";
-        else if (coin.equalsIgnoreCase("doge")) return "dogecoin";
-        else if (coin.equalsIgnoreCase("matic")) return "polygon";
-        return coin;
+    static {
+        Request request = new Request.Builder()
+                .url("https://api.coingecko.com/api/v3/coins/list")
+                .build();
+        Gson gson = new Gson();
+        try (Response response = CLIENT.newCall(request).execute()) {
+            JsonArray array = gson.fromJson(response.body().string(), JsonArray.class);
+            array.forEach((e) -> COIN_DATA_LIST.add(gson.fromJson(e, CoinData.class)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<String> formatCoin(String coin) {
+        List<String> selectedCoins = Arrays.stream(coin.replaceAll("\\s+", "").split(",")).toList();
+
+        return selectedCoins.stream().map(
+                (c) ->
+                        COIN_DATA_LIST.stream().filter(
+                                (f) -> f.getSymbol().equalsIgnoreCase(c)
+                        ).toList().get(0).getId()
+
+        ).toList();
     }
 
     public static String getCryptoPrice(String coin, String vsUnit) {
@@ -29,4 +51,23 @@ public class CryptoUtils {
         }
         return null;
     }
+
+    public static class CoinData {
+        private String id;
+        private String name;
+        private String symbol;
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+    }
+
 }
