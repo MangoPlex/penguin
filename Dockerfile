9f0dcb9aa29a490f5defa12fe30792dad8b3a1a5
JustAPie <justapie06@gmail.com>
2023-08-19 12:46:43 +0700

    set system properties as env vars

--- Dockerfile
+++ Dockerfile
@@ -8,12 +8,6 @@ ENV DISCORD_TOKEN=${DISCORD_TOKEN}
 ENV DATABASE_NAME=${DATABASE_NAME}
 ENV DATABASE_URL=${DATABASE_URL}
 
-WORKDIR /usr/Penguin
-
-COPY build.gradle build.gradle
-COPY settings.gradle settings.gradle
-COPY src src
-
 RUN ["gradle", "build"]
 
 CMD ["java", "-jar", "build/libs/Penguin-1.0.0-all.jar"]
--- src/main/java/xyz/mangostudio/penguin/Config.java
+++ src/main/java/xyz/mangostudio/penguin/Config.java
@@ -3,7 +3,7 @@ package xyz.mangostudio.penguin;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
-    private static final Dotenv DOTENV = Dotenv.load();
+    private static final Dotenv DOTENV = Dotenv.configure().systemProperties().load();
 
     public static String getConfig(String key) {
         String val = DOTENV.get(key);
